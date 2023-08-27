9235c77dbcc6e1e5b89d78075bdd8874ff7e7ef3
Githubuser289 <133155777+Githubuser289@users.noreply.github.com>
2023-08-27 05:21:13 +0000

    Deploying to gh-pages from @ Githubuser289/All4Team-SimplyChocolate@79bdc79ccc4f873c36f7b6d8f505d21cad48efe7 🚀

---
+++ index.13b417c2.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-hero]"),closeModalBtn:document.querySelector("[data-modal-close-hero]"),modal:document.querySelector("[data-modal-hero]")};function o(){e.modal.classList.toggle("is-hidden-hero")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.13b417c2.js.map
